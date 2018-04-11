ad9ba8df5963328f4a90643b9ff56064582152f1
BEGGSYLAPTOP\chris <layzdaygamer@gmail.com>
2018-04-11 18:08:34 +0400

    my shit

---
+++ CRGame/Assets/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+
+        }
+    }
+}
